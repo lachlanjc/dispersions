b43897494280a7940b632259db54afd58b00283b
Lachlan Campbell <lachlanjc@hey.com>
2021-02-15 15:15:18 -0500

    Add Vimeo embed

--- components/icons.tsx
+++ components/icons.tsx
@@ -56,6 +56,7 @@ export const ArrowLeft = ({ ...props }) => (
     height="24"
     viewBox="0 0 24 24"
     fill="none"
+    xmlns="http://www.w3.org/2000/svg"
     strokeWidth={1.5}
     stroke="currentColor"
     {...props}
--- pages/exhibition.tsx
+++ pages/exhibition.tsx
@@ -1,16 +1,23 @@
 import { Grid, Box, Button, Flex, Container, Heading } from 'theme-ui'
 import Image from 'next/image'
 import NextLink from 'next/link'
+import Vimeo from '@u-wave/react-vimeo'
 import Meta from '@/components/meta'
 import BGImg from '@/components/bg-img'
 import { ArrowRight, ChevronNext } from '@/components/icons'
+import theme from '@/lib/theme'
 
 const Exhibition = () => (
   <>
     <Meta title="Intro" />
     <Flex
       as="header"
-      sx={{ position: 'relative', minHeight: '97vh', placeItems: 'center' }}
+      sx={{
+        position: 'relative',
+        minHeight: '97vh',
+        bg: 'secondary',
+        placeItems: 'center',
+      }}
     >
       <BGImg
         alt="Detail of Christopher's drawing with graphite in dripping arcs"
@@ -45,7 +52,7 @@ const Exhibition = () => (
           as="h1"
           variant="ultratitle"
           sx={{
-            fontSize: [5, 6, 7, 8],
+            fontSize: [4, 6, 7, 8],
             letterSpacing: '.15em',
             textTransform: 'uppercase',
             mb: [4, 5],
@@ -68,6 +75,30 @@ const Exhibition = () => (
           </Button>
         </NextLink>
       </Container>
+    </Flex>
+    <Flex
+      as="section"
+      sx={{
+        px: [null, 4, 5],
+        py: 5,
+        bg: 'black',
+        color: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        '> div, iframe': {
+          maxWidth: '95vw',
+          width: [384, 512, 1024],
+          height: [384, 512, 1024].map(n => n * (9 / 16)),
+        },
+      }}
+    >
+      <Vimeo
+        video="https://vimeo.com/512374691/23becfae57"
+        color={theme?.colors?.accent?.toString().replace('#', '')}
+        showTitle={false}
+        showByline={false}
+        showPortrait={false}
+      />
     </Flex>
   </>
 )
