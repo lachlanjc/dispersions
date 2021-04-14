ed07ef1e27f97204f50dd16aeb15951ad51d878b
Lachlan Campbell <lachlanjc@hey.com>
2021-04-13 22:35:10 -0400

    Begin excerpts

--- pages/exhibition.tsx
+++ pages/exhibition.tsx
@@ -5,10 +5,63 @@ import Vimeo from '@u-wave/react-vimeo'
 import Meta from '@/components/meta'
 import BGImg from '@/components/bg-img'
 import Video from '@/components/video'
+import Abstract from '@/components/writing/abstract.mdx'
+import ExcerptThirtyThree from '@/components/writing/excerpt-33.mdx'
+import ExcerptFourtyEight from '@/components/writing/excerpt-48.mdx'
 import { ArrowRight, ChevronNext } from '@/components/icons'
 import { colors } from '@/lib/theme'
+import { getWork } from '@/lib/worklist'
+import Work from '@/components/work'
 
-const Exhibition = () => (
+const StickyWork = ({ work }: { work: Artwork }) => (
+  <Box
+    as="section"
+    sx={{ p: [3, 4], position: 'sticky', top: 0, width: '100%', bg: 'white' }}
+  >
+    <Container variant="narrow">
+      <Work work={work} />
+    </Container>
+  </Box>
+)
+
+const Waterfall = ({ children }: { children: any }) => {
+  return (
+    <Container
+      as="section"
+      variant="copy"
+      sx={{
+        ul: {
+          p: 0,
+          mt: [4, 5],
+          listStyle: 'none',
+          display: 'grid',
+          // gridTemplateColumns: [null, 'repeat(2, 1fr)'],
+          gridGap: [3, 4],
+        },
+        li: {
+          fontSize: 2,
+          display: 'flex',
+          alignItems: 'center',
+          '&:nth-of-type(even)': {
+            ml: [null, -4, -5],
+          },
+        },
+        img: {
+          order: -1,
+          mr: [3, 4],
+          display: 'block',
+          maxHeight: 400,
+          maxWidth: 400,
+          mx: 'auto',
+        },
+      }}
+    >
+      {children}
+    </Container>
+  )
+}
+
+const Exhibition = ({ works }: { works: Artwork[] }) => (
   <>
     <Meta title="Intro" />
     <Flex
@@ -77,43 +130,63 @@ const Exhibition = () => (
         </NextLink>
       </Container>
     </Flex>
-    <Flex
+    <Box
       as="section"
       sx={{
-        px: [null, 4, 5],
         py: 5,
         bg: 'black',
-        color: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
-        minHeight: '100vh',
-        '> div': {
-          display: 'block',
-          width: '95vw',
-          maxWidth: [384, 512, 1024, 1536],
-          height: 0,
-          paddingBottom: `${(9 / 16) * 100}%`,
-          position: 'relative',
-        },
-        iframe: {
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-          top: 0,
-          bottom: 0,
-          left: 0,
-          border: 0,
-        },
       }}
     >
-      <Vimeo
-        video="https://vimeo.com/514663630/4c2568af77"
-        color={colors.red.toString().replace('#', '')}
-        showTitle={false}
-        showByline={false}
-        showPortrait={false}
-      />
-    </Flex>
+      <Box
+        sx={{
+          px: [null, 4, 5],
+          mx: 'auto',
+          '> div': {
+            display: 'block',
+            width: '100%',
+            mx: 'auto',
+            maxWidth: [384, 512, null, 1024],
+            height: 0,
+            paddingBottom: `${(9 / 16) * 100}%`,
+            position: 'relative',
+          },
+          iframe: {
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            border: 0,
+          },
+        }}
+      >
+        <Vimeo
+          video="https://vimeo.com/514663630/4c2568af77"
+          color={colors.red.toString().replace('#', '')}
+          showTitle={false}
+          showByline={false}
+          showPortrait={false}
+        />
+      </Box>
+      <Container variant="copy" sx={{ p: { color: 'white' }, fontSize: 2 }}>
+        <Abstract />
+      </Container>
+    </Box>
+    <Box as="section" sx={{ py: [4, 5] }}>
+      <Grid variant="layout.container" gap={[4, 5]} columns={[null, '2fr 3fr']}>
+        <Work work={works.fourtyEight} img={1} />
+        <Container variant="copy" sx={{ fontSize: 2 }}>
+          <ExcerptFourtyEight />
+        </Container>
+      </Grid>
+      <Container>
+        <StickyWork work={works.thirtyThree} />
+        <Waterfall>
+          <ExcerptThirtyThree />
+        </Waterfall>
+      </Container>
+    </Box>
     <Video
       mux="rlyfZBce1Bne42x00hISDPmI9vHZJmgqAnQkBw01UnnP4"
       width="100%"
@@ -125,3 +198,11 @@ const Exhibition = () => (
 )
 
 export default Exhibition
+
+export async function getStaticProps() {
+  const works = {
+    thirtyThree: getWork('2.2020.039'),
+    fourtyEight: getWork('2.2020.057'),
+  }
+  return { props: { works } }
+}
