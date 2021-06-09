c639ded186d450e8133eaeca47ba55ff5a65a9cc
Lachlan Campbell <lachlanjc@hey.com>
2021-06-09 14:36:59 -0700

    Remove webpack5 flag

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
 module.exports = withMDX({
-  future: {
-    webpack5: true,
-  },
   pageExtensions: ['js', 'ts', 'tsx', 'mdx'],
   images: {
     domains: ['images.squarespace-cdn.com', 'd1wa56x8uvnqfp.cloudfront.net'],
